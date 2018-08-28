b2ffd24a47fbbdc97f88e390fd495209697c8ef5
ZhangWei <350095093@qq.com>
2018-08-28 13:34:49 +0800

    config css style

--- pages/home.js
+++ pages/home.js
@@ -1,16 +1,21 @@
 import React,{Component} from "react";
 import Head from "next/head";
 import { withRouter } from "next/router";
+import { Button } from "antd";
+import "../style.css";
+
 class Home extends Component{
     render(){
         return(
-            <div lang="zh-Hans">
+            <div>
                 <Head>
                     <title>My page title</title>
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                     <meta name="description" content="张伟JS个人网站"/>
                 </Head>
                 <h2>zhangwei</h2>
+                <div className="example">sasa</div>
+                <Button>SADASD</Button>
             </div>
         );
     }
