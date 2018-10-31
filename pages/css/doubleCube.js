028d3e855238c84c80dec8fdf4f59c714762ce67
ZhangWei <350095093@qq.com>
2018-10-31 14:10:10 +0800

    add css course

---
+++ pages/css/doubleCube.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./doubleCube.css";
+
+const doubleCube = () =>{
+  return(
+    <div>
+      <div className="spinner">
+        <div className="cube1"></div>
+        <div className="cube2"></div>
+        <div className="cube3"></div>
+        <div className="cube4"></div>
+      </div>
+    </div>
+  );
+};
+
+export default doubleCube;
